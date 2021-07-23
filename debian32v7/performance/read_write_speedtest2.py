1cb3f4307cc7295a37599129cb426976a5b1a436
Eggo5 <rthem@gmx.de>
2021-07-23 21:00:13 +0200

    get output trough file v4

--- debian32v7/performance/read_write_speedtest2.py
+++ debian32v7/performance/read_write_speedtest2.py
@@ -11,7 +11,7 @@ with open(filename) as f:
     content = f.readlines()
 
 # Schreiben des Inhalts in eine Datei
-o = open("passwords_output")
+o = open('passwords_output', 'w')
 for line in content:
     o.write(line)
 o.close()
