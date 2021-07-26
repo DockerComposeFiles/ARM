cede18452d3eb1eb95bba1d414511a09b3ac71b5
Eggo5 <rthem@gmx.de>
2021-07-26 23:15:09 +0200

    seperate performance for raspi and beagle

---
+++ alpine32v7/performance/read_write_speedtest2.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+# Auslesen einer Datei
+import sys
+import time
+
+# Messen der Lesezeiten
+readStart = time.time()
+
+# Testdatei
+readFile = "passwords"
+# Datei wird in f geöffnet und gelesen
+with open(readFile) as f:
+    content = f.readlines()
+f.close()
+
+# Berechnung der Lesezeiten
+readTime = (time.time() - readStart)
+print('readTime:')
+print(readTime)
+# erzwingen der Ausgabe
+sys.stdout.flush()
+
+# Messen der Schreibzeiten
+writeStart = time.time()
+
+# Schreiben des Inhalts in eine Datei
+o = open('passwords_output', 'w')
+for line in content:
+    o.write(line)
+o.close()
+
+# Berechnung der Schreibzeiten
+writeTime = (time.time() - writeStart)
+print('writeTime:')
+print(writeTime)
+# erzwingen der Ausgabe
+sys.stdout.flush()
+
+# Laufen lassen des Containers für 200 Sekunden
+# time.sleep(200)
