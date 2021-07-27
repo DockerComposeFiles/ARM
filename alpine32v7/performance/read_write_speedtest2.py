913f24c39fd40b8336f1c8ecddc839f3fe7ff69b
Eggo5 <rthem@gmx.de>
2021-07-27 11:55:16 +0200

    alpine:performance test

--- alpine32v7/performance/read_write_speedtest2.py
+++ alpine32v7/performance/read_write_speedtest2.py
@@ -1,43 +1,6 @@
-#!/usr/bin/env python
-
-# Auslesen einer Datei
-import sys
-import time
 import datetime
+import time
 
-# Messen der Lesezeiten
 print(datetime.date)
-readStart = time.time()     #Operation wird nicht erlaubt
-
-# Testdatei
-readFile = "passwords"
-# Datei wird in f geöffnet und gelesen
-with open(readFile) as f:
-    content = f.readlines()
-f.close()
-
-# Berechnung der Lesezeiten
-readTime = (time.time() - readStart)
-print('readTime:')
-print(readTime)
-# erzwingen der Ausgabe
-sys.stdout.flush()
-
-# Messen der Schreibzeiten
-writeStart = time.time()
-
-# Schreiben des Inhalts in eine Datei
-o = open('passwords_output', 'w')
-for line in content:
-    o.write(line)
-o.close()
-
-# Berechnung der Schreibzeiten
-writeTime = (time.time() - writeStart)
-print('writeTime:')
-print(writeTime)
-# erzwingen der Ausgabe
-sys.stdout.flush()
-
 # Laufen lassen des Containers für 200 Sekunden
-# time.sleep(200)
+time.sleep(200)
---
+++ alpine32v7/performance/temp.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# Auslesen einer Datei
+import sys
+import time
+
+# Messen der Lesezeiten
+readStart = time.time()     #Operation wird nicht erlaubt
+
+# Testdatei
+readFile = "passwords"
+# Datei wird in f geöffnet und gelesen
+with open(readFile) as f:
+    content = f.readlines()
+f.close()
+
+# Berechnung der Lesezeiten
+readTime = (time.time() - readStart)
+print('readTime:')
+print(readTime)
+# erzwingen der Ausgabe
+sys.stdout.flush()
+
+# Messen der Schreibzeiten
+writeStart = time.time()
+
+# Schreiben des Inhalts in eine Datei
+o = open('passwords_output', 'w')
+for line in content:
+    o.write(line)
+o.close()
+
+# Berechnung der Schreibzeiten
+writeTime = (time.time() - writeStart)
+print('writeTime:')
+print(writeTime)
+# erzwingen der Ausgabe
+sys.stdout.flush()
+
