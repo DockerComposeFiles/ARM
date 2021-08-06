726c32daac025312d495490f77b542faeb5f6692
Eggo5 <rthem@gmx.de>
2021-08-06 16:51:40 +0200

    comment display output

--- debian32v7/GPIO/script.sh
+++ debian32v7/GPIO/script.sh
@@ -2,7 +2,10 @@
 # Dieses Skript gibt die über i2C erkannten GPIO Geraete aus.
 
 # Zeigt installierte BUS Adapter an
+echo "installed i2c adapters:"
 i2cdetect -l
+echo
+echo "connected devices:"
 # Scannt den I2C Adressbereich und zeigt I2C antworten an
 i2cdetect -y 1
 
