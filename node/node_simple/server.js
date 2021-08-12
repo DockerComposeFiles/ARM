89e705f0186924d6d80246ebd859a0e2db819a61
Ruben <Rthem@gmx.de>
2021-08-12 22:07:40 +0200

    NODE: sep node_5

--- node/node_simple/server.js
+++ node/node_simple/server.js
@@ -3,8 +3,8 @@
 const express = require('express');
 
 // Constants
-const PORT = 8081;
-const HOST = '172.25.1.2';
+//const PORT = 8080;
+//const HOST = '172.25.1.2';
 
 // App
 const app = express();
@@ -13,4 +13,4 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, HOST);
-console.log(`Running on http:slash_slash${HOST}:${PORT}`);
+console.log(`Server is Running on http://${HOST}:${PORT}`);
