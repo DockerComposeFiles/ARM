e00c69d3307e51afac9dd94c4fcffee935649ce5
Ruben <Rthem@gmx.de>
2021-08-27 12:45:20 +0200

    BMP280_oneData19_Socket

--- visualize/combined/script.py
+++ visualize/combined/script.py
@@ -11,7 +11,7 @@ while not fileObj.is_file:
     print("no_File")
     time.sleep(5)
 if fileObj.is_file:
-    print("file exist")
+    print("file exist:" + fileObj + fileName)
 
 # Einlesen der Daten in einer Endlosschleife
 i = 0
