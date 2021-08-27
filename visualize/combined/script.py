f3a1083fbede5a84f105ca88eed727020523cf53
Ruben <Rthem@gmx.de>
2021-08-27 12:04:37 +0200

    BMP280_oneData16_Socket

--- visualize/combined/script.py
+++ visualize/combined/script.py
@@ -1,6 +1,16 @@
 import time
+from pathlib import Path
 
 print("reading start")
+
+# Wenn Datei nicht existiert, ist der Sensor_Container noch nicht Einsatzbereit
+fileName = r"/data/data.txt"
+fileObj = Path(fileName)
+# Warten auf anderen Container
+
+while not fileObj.is_file:
+    time.sleep(5)
+
 # Endlosschleife
 i = 0
 while i <= 1:
