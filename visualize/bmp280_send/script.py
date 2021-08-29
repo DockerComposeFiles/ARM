873f3f5516e87d56f96464ffbde5500e889688e9
Ruben <Rthem@gmx.de>
2021-08-29 11:32:19 +0200

    BMP280_volumeConfig_Socket two files_5

--- visualize/bmp280_send/script.py
+++ visualize/bmp280_send/script.py
@@ -12,13 +12,13 @@ temperatur, druck, x = bme280.readBME280All()
 
 # Daten in volume schreiben
 fo = open('/data/data.txt', 'w')
-fo.write(str(temperatur))
-fo.write(str(druck))
+fo.write(str(temperatur) + "\r\n")
+fo.write(str(druck) + "\r\n")
 fo.close()
 
 fa = open('/data/all_data.txt', 'a')
-fa.write((str(temperatur)))
-fa.write(str(druck))
+fa.write(str(temperatur) + "\r\n")
+fa.write(str(druck) + "\r\n")
 fa.close()
 
 print("write_finish")
