5fb73c989581c535dfe7ee0757f3d5d877493fa4
Ruben <Rthem@gmx.de>
2021-09-03 17:25:27 +0200

    combined_websocket 11

--- visualize/combined/send_data.py
+++ visualize/combined/send_data.py
@@ -4,16 +4,12 @@
 import asyncio
 import websockets
 
-async def hello(uri):
+async def send_current(uri):
     async with websockets.connect(uri) as websocket:
         await websocket.send("Hello world!")
         await websocket.recv()
 
-try:
-    asyncio.get_event_loop().run_until_complete(
-        hello('ws://localhost:80'))
-    print("script.js finish")
 
-except:
-    exec
-
+asyncio.get_event_loop().run_until_complete(
+    send_current('172.20.0.2:80'))
+print("script.js finish")
