016229053dbc140398fea7eb89766c87efd33eda
Ruben <Rthem@gmx.de>
2021-09-03 20:09:06 +0200

    combined_websocket connection

---
+++ visualize/combined_python/send_data.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+import socketio
+
+# standard Python
+sio = socketio.Client()
+
+# asyncio
+asio = socketio.AsyncClient()
+
+
+# Event handler
+@sio.on('*')
+def catch_all(event, sid, data):
+    pass
+
+
+@sio.on('*')
+async def catch_all(event, sid, data):
+    pass
+
+
+@sio.event
+def connect():
+    print("I'm connected!")
+
+
+@sio.event
+def connect_error(data):
+    print("The connection failed!")
+
+
+@sio.event
+def disconnect():
+    print("I'm disconnected!")
+
+
+sio.connect('http://localhost:3000')
