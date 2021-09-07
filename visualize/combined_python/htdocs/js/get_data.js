1f9986d33e3cc850688cc63aaf67b5dc0eee04c7
Ruben <Rthem@gmx.de>
2021-09-07 17:40:10 +0200

    combined_websocket connection_10

--- visualize/combined_python/htdocs/js/get_data.js
+++ visualize/combined_python/htdocs/js/get_data.js
@@ -8,10 +8,6 @@ const io = new socket_io_1.Server(httpServer, {
 });
 io.on("connection", (socket) => {
     console.log("IN FUNCTION: io.on(connection)");
-    const uuid = require("uuid");
-    io.engine.generateId = (req) => {
-        return uuid.v4;
-    };
     socket.on('disconnect', function () {
         console.log("Verbindung geschlossen");
     });
