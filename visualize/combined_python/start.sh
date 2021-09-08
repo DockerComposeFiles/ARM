e27062be0ab30294f9356a95f9069c1729eb87d4
Ruben <Rthem@gmx.de>
2021-09-08 21:07:34 +0200

    combined_websocket connection_57

--- visualize/combined_python/start.sh
+++ visualize/combined_python/start.sh
@@ -15,19 +15,5 @@ echo "start Apache fin"
 sleep 10
 echo "10 sec vergangen"
 
-# Python Backend
-python3 /send_data.py
-status=$?
-if [ $status -ne 0 ]; then
-  echo "Failed to start Backend: $status"
-  exit $status
-else echo "Python-Backend run"
-fi
 
-echo "start backend fin"
-sleep 10
-echo "2 x 10 sec vergangen"
-
-# nohub Schreibweise
-#nohup python3 ./send_data &
 echo "start.sh_finished"
