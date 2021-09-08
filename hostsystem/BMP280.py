560144afd1ce21d94ec237884aa84c48fd11e5de
Ruben <Rthem@gmx.de>
2021-09-08 09:49:52 +0200

    combined_websocket connection_18 (hostsystem API)

--- hostsystem/BMP280.py
+++ hostsystem/BMP280.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import bme280
 
-#Messdaten Holen
-temperatur,druck, x = bme280.readBME280All()
-print ("Temperatur : ", temperatur, "C")
-print ("Druck: ", druck, "hPa")
+# Messdaten Holen
+temperatur, druck, x = bme280.readBME280All()
+print("Temperatur : ", temperatur, "C")
+print("Druck: ", druck, "hPa")
--- hostsystem/raspiGPIO.py
+++ hostsystem/raspiGPIO.py
@@ -6,4 +6,3 @@ import RPi.GPIO as GPIO
 GPIO.cleanup()
 
 # Denken Sie daran, dass ein Programm nicht unbedingt mit der letzten Zeile endet.
-# remember, a program doesn't necessarily exit at the last line!
