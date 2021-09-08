e464cbdb791eb8da2d494e5e51572005c9808014
Ruben <Rthem@gmx.de>
2021-09-08 21:53:22 +0200

    combined_websocket connection_58

--- visualize/combined_python/start.sh
+++ visualize/combined_python/start.sh
@@ -3,17 +3,31 @@
 # um beide Prozesse zu starten
 
 # Apache Frontend
-/usr/local/apache2/bin/apachectl &
+#/usr/local/apache2/bin/apachectl &
+#status=$?
+#if [ $status -ne 0 ]; then
+#  echo "Failed to start Apache_Frontend: $status"
+#  exit $status
+#else echo "Apache arbeitet"
+#fi
+#
+#echo "start Apache fin"
+#sleep 10
+#echo "10 sec vergangen"
+
+# Python Backend
+python3 /send_data.py &
 status=$?
 if [ $status -ne 0 ]; then
-  echo "Failed to start Apache_Frontend: $status"
+  echo "Failed to start Backend: $status"
   exit $status
-else echo "Apache arbeitet"
+else echo "Python-Backend run"
 fi
 
-echo "start Apache fin"
+echo "start backend fin"
 sleep 10
-echo "10 sec vergangen"
+echo "2 x 10 sec vergangen"
 
-
+# nohub Schreibweise
+#nohup python3 ./send_data &
 echo "start.sh_finished"
