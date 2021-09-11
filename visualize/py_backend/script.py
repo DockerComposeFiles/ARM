cff8314faf49420ceca76568d6e026955135d469
Ruben <Rthem@gmx.de>
2021-09-12 01:53:06 +0200

    bmp280 46 backend_connect_frontend

--- visualize/py_backend/script.py
+++ visualize/py_backend/script.py
@@ -39,12 +39,12 @@ def disconnect():
 # time.sleep(20)
 
 # Testen, welche Verbindungen (Apache Server) verfügbar sind
-# Internet ok, Localhost=127.0.0.1 ok, 127.24.0.5 ok
+# Internet down?, Localhost=127.0.0.1 ok, 127.24.0.5 ok
 # 0.0.0.0 ok, 172.24.0.0-subnet ok, 172.24.0.1-gateway ok
 # Der Websocket läuft auf Port 3000
 
 IPs = ["www.google.com", "localhost", '127.24.0.0', '127.24.0.1', '172.24.0.5', '0.0.0.0',
-       '127.24.0.0:3000', '127.24.0.1:3000', '172.24.0.5:3000', '0.0.0.0:3000']
+       '172.24.0.5:3000', '172.24.0.2', '172.24.0.2:3000']
 
 for e in IPs:
     # Ping Abfrage, um die Verfügbarkeit zu testen
