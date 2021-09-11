5bc51cc812a3ab8f60e92bc1624cffde7960817b
Ruben <Rthem@gmx.de>
2021-09-11 23:45:36 +0200

    bmp280 28 backend_connect_frontend

--- visualize/py_backend/script.py
+++ visualize/py_backend/script.py
@@ -38,26 +38,24 @@ def disconnect():
 
 # Warten, damit Apache bereit ist
 time.sleep(20)
-print("go to ip test", flush=True)
-
+print("go to ip array", flush=True)
 # Testen, ob Localhost (127.0.0.0, 127.0.0.1),
 # die feste IP 172.24.0.2 und ? 0.0.0.0 verfügbar ist.
 # Zugriff auf den Apache Server wird getestet
 # Der Websocket läuft auf Port 3000
 
-#IPs = ['localhost', '127.0.0.0', '127.0.0.1', '172.24.0.2', '0.0.0.0',
-#       '127.0.0.0:3000', '127.0.0.1:3000', '172.24.0.2:3000', '0.0.0.0:3000']
-#
-#for e in IPs:
-#    response = os.system("ping -n 1 " + e)
-#
-#    if response == 0:
-#        print(e, 'is up', flush=True)
-#    else:
-#        print(e, 'is down', flush=True)
+IPs = ['localhost', '127.0.0.0', '127.0.0.1', '172.24.0.2', '0.0.0.0',
+       '127.0.0.0:3000', '127.0.0.1:3000', '172.24.0.2:3000', '0.0.0.0:3000']
+print("go to ip test", flush=True)
+for e in IPs:
+    response = os.system("ping -n 1 " + e)
+
+    if response == 0:
+        print(e, 'is up', flush=True)
+    else:
+        print(e, 'is down', flush=True)
 
 print("go to connection", flush=True)
-
 # Mit Host Verbinden
 sio.connect('http://172.24.0.2:3000/')
 
