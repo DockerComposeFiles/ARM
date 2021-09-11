10265865447c25b2e2f2785b6afd2735ae272a35
Ruben <Rthem@gmx.de>
2021-09-12 01:06:48 +0200

    bmp280 38 backend_connect_frontend

--- visualize/py_backend/script.py
+++ visualize/py_backend/script.py
@@ -46,8 +46,8 @@ IPs = ['www.google.de', '127.0.0.0', '127.0.0.1', '172.24.0.2', '0.0.0.0',
        '127.0.0.0:3000', '127.0.0.1:3000', '172.24.0.2:3000', '0.0.0.0:3000', 'localhost']
 
 for e in IPs:
-    print("proof in the array: " + e, flush=True)
-    response = os.system('ping -n -c 1 1 ' + e)
+    response = os.system('ping -n 1 ' + e)
+    print(e + "was proof in the array: " + response, flush=True)
 
     # Standard Zeitintervall zwischen den PING Ausführungen
     time.sleep(1)
