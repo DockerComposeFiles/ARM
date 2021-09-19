eb08528cddc67de36382a92fb5ff77e2083304a0
Ruben <Rthem@gmx.de>
2021-09-19 18:45:30 +0200

    visualizer_websocket_13 (GPIO BBB18

--- hostsystem/beaglebone/gpio_analyse.sh
+++ hostsystem/beaglebone/gpio_analyse.sh
@@ -10,11 +10,12 @@ echo
 # Abfrage der erkannten GPIO
 ls -al /sys/class/gpio
 echo
-# Abfrage, ob pin 1 in "gpio mode" geschaltet ist.
-echo "ask single Pin 1 of gpio-mode:"
-echo 4 > /sys/kernel/debug/omap_mux/gpmc_ad4
-echo 17 > /sys/kernel/debug/omap_mux/gpmc_ad4
-echo
+
+# Abfrage, ob bestimmte pin in "gpio mode" geschaltet ist.
+#echo "ask single Pin 4 and 17 of gpio-mode:"
+#echo 4 > /sys/kernel/debug/omap_mux/gpmc_ad4
+#echo 17 > /sys/kernel/debug/omap_mux/gpmc_ad4
+#echo
 
 ## PIN Abfragen
 # Abfragen uber Debug Informationen
@@ -29,11 +30,11 @@ cat /sys/kernel/debug/pinctrl/44e3e000.rtc/pinconf-config |more
 ## I2C Abfragen
 # Zeigt installierte BUS Adapter an
 echo "installed i2c adapters:"
-#i2cdetect -l
+i2cdetect -l
 echo
 echo "connected devices:"
 # Scannt den I2C Adressbereich und zeigt I2C antworten von Geraeten an
-#i2cdetect -y 1
+i2cdetect -y 1
 echo
 ## Angeschlossene USB-Geräte anzeigen lassen
 lsusb
