13cdba5a613f12086a9423edeb679cb8c7f7688b
Ruben <Rthem@gmx.de>
2021-09-19 19:00:39 +0200

    visualizer_websocket_13 (GPIO BBB20

--- BBB/config.sh
+++ BBB/config.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
-echo 48 > /sys/class/gpio/export
-echo 50 > /sys/class/gpio/export
-echo 51 > /sys/class/gpio/export
-echo 60 > /sys/class/gpio/export
+#echo 48 > /sys/class/gpio/export
+#echo 50 > /sys/class/gpio/export
+#echo 51 > /sys/class/gpio/export
+#echo 60 > /sys/class/gpio/export
 
+config-pin p9.17 gpio # normal
+config-pin p9.18 gpio # normal
+
+config-pin p9.19 gpio # shared
+config-pin p9.20 gpio # shared
+
+# konfiguriere testpin 7 an P8
+config-pin p8.17 gpio # shared
+
+exit 0
