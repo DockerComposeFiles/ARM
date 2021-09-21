9081aeb68b5960bdfcba14304cc08236113de84d
Ruben <Rthem@gmx.de>
2021-09-21 16:42:30 +0200

    BBB5

--- hostsystem/beaglebone/config_htu.sh
+++ hostsystem/beaglebone/config_htu.sh
@@ -6,13 +6,15 @@
 #echo 19 > /sys/class/gpio/export
 #echo 20 > /sys/class/gpio/export
 
-# konfiguriere I2C PINS
-config-pin p9.1 gpio # Ground
-config-pin p9.3 gpio # 3,3V Power
+# Anschlüsse
+# p9.1 gpio # Ground
+# p9.3 gpio # 3,3V Power
 
+# konfiguriere I2C PINS
 config-pin p9.19 gpio # shared i2c Pin
 config-pin p9.20 gpio # shared i2c Pin
 
+# Moeglicherweise alternativen
 #config-pin p9.17 gpio # normaler i2c Pin
 #config-pin p9.18 gpio # normaler i2c Pin
 
