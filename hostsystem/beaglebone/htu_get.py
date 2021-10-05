7cff5ba5fab64a6a94196076bfc4c1b68d0a837d
Ruben <Rthem@gmx.de>
2021-10-05 13:34:33 +0200

    BBB GPIO_22

---
+++ hostsystem/beaglebone/htu_get.py
@@ -0,0 +1,13 @@
+import time
+import board
+from adafruit_htu21d import HTU21D
+
+# Erstellt ein Sensor Objekt, um den I2C standard bus zu kommunizieren.
+i2c = board.I2C()  # Nutzt board.SCL und board.SDA
+sensor = HTU21D(i2c)
+
+
+while True:
+    print("\nTemperature: %0.1f C" % sensor.temperature)
+    print("Humidity: %0.1f %%" % sensor.relative_humidity)
+    time.sleep(2)
--- projects/Lc-Display/b2/script.py
+++ projects/Lc-Display/b2/script.py
@@ -1,2 +1,26 @@
+# auslesen
+
+import os
+
+import Adafruit_BBIO.GPIO as GPIO
+
+# Beispiel
+#GPIO.setup("P8_10", GPIO.OUT)
+#GPIO.output("P8_10", GPIO.HIGH)
+
+# konfiguriere I2C Pins direkt
+GPIO.setup("P9_17", GPIO.IN)
+GPIO.setup("P9_18", GPIO.IN)
+GPIO.setup("P9_19", GPIO.IN)
+GPIO.setup("P9_20", GPIO.IN)
+GPIO.setup("P9_21", GPIO.IN)
+GPIO.setup("P9_22", GPIO.IN)
+
+GPIO.setup("P9_24", GPIO.IN)
+GPIO.setup("P9_22", GPIO.IN)
+
+# Löschen der Konfiguration
+GPIO.cleanup()
+
 # speichern
-#/htu_data/b2
+#/htu_data/b1
