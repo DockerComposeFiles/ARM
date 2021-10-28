1a3ae5c4fc971cddd5340f16c9aee6bbc988ac57
Ruben <Rthem@gmx.de>
2021-10-28 17:55:30 +0200

    auto_deploy.py 4

--- projects/Automation/auto_deploy.py
+++ projects/Automation/auto_deploy.py
@@ -12,7 +12,7 @@ htu21d_ = True
 # Wenn Sensor 1
 def bmp180():
     x = os.system("i2cdetect -y 1")
-    if x.contains("77"):
+    if x.contains(77):
         os.system("docker run --device /dev/i2c-1 myimage")
     print("bmp180 Container will deploy", flush=True)
 
@@ -21,7 +21,7 @@ def bmp180():
 
 def bmp280():
     x = os.system("i2cdetect -y 1")
-    if x.contains("76"):
+    if x.contains(76):
         os.system("docker run --device /dev/i2c-1 myimage")
     print("bmp280 Container will deploy", flush=True)
 
@@ -30,7 +30,7 @@ def bmp280():
 
 def htu21d():
     x = os.system("i2cdetect -y 1")
-    if x.contains("40"):
+    if x.contains(40):
         os.system("docker run --device /dev/i2c-1 myimage")
         print("htu21d Container will deploy", flush=True)
 
