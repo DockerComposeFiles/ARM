60be470cc927d54292059c0dd00b988a1d80251c
Ruben <Rthem@gmx.de>
2021-11-06 18:08:30 +0100

    auto_deploy.py_18

--- projects/Automation/auto_deploy.py
+++ projects/Automation/auto_deploy.py
@@ -12,7 +12,7 @@ bmp280_ = True
 # Deploy Funktionen BMP180
 def bmp180():
     bmp180_scan = os.system("i2cget -y 1 0x77")
-    if bmp180_scan == "0x00\n0":
+    if bmp180_scan == "0x00":
         # print("bmp180 Container will download", flush=True)
         # os.system("docker pull 326567/bmp180")
         print("bmp180 Container will deploy", flush=True)
@@ -31,7 +31,7 @@ def bmp180():
 # Deploy Funktionen BMP280
 def bmp280():
     bmp280_scan = os.system("i2cget -y 1 0x76")
-    if bmp280_scan == "0x00\n0":
+    if bmp280_scan == "0x00":
         # print("bmp280 Container will download", flush=True)
         # os.system("docker pull 326567/bmp280")
         print("bmp280 Container will deploy", flush=True)
@@ -40,8 +40,8 @@ def bmp280():
     elif bmp280_scan == "Error: Read failed\n512":
         print("bmp180 no connection by 512", flush=True)
 
-    elif bmp280_scan == "Error: Read failed\n589":
-        print("bmp180 no connection by 589", flush=True)
+    elif bmp280_scan == "Error: Read failed\n588":
+        print("bmp180 no connection by 588", flush=True)
 
     else:
         print("bmp280 no connection: unknown message", flush=True)
