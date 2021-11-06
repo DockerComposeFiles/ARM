ecadf858b6f8cd2c243c55d2cae679c4756de1c4
Ruben <Rthem@gmx.de>
2021-11-06 16:07:38 +0100

    auto_deploy.py_9

--- projects/Automation/scan_test.py
+++ projects/Automation/scan_test.py
@@ -19,16 +19,19 @@ print(os.system("i2cget -y 1 0x40"), flush=True)
 print("\n", flush=True)
 
 # Scann des 1 Sensors
-obj77 = os.system("i2cget -y 1 0x77")
+obj77 = os.system("i2cget -y 1 0x77 0x11")
 res77 = object_converter(obj77)
 print(77 + res77, flush=True)
+# Ergebnis: 589
 
 # Scann des 1 Sensors
-obj76 = os.system("i2cget -y 1 0x76")
+obj76 = os.system("i2cget -y 1 0x76 0x11")
 res76 = object_converter(obj76)
 print(76 + res76, flush=True)
+# Ergebnis: 588
 
 # Scann des 3 Sensors
-obj40 = os.system("i2cget -y 1 0x40")
+obj40 = os.system("i2cget -y 1 0x40 0x11")
 res40 = object_converter(obj40)
 print(40 + res40, flush=True)
+# Ergebnis: 552
