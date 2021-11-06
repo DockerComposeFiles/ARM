a31239bba476fe43207781980a09abec423785e1
Ruben <Rthem@gmx.de>
2021-11-06 18:47:55 +0100

    auto_deploy.py_Seitenumbruch

--- projects/Automation/auto_deploy.py
+++ projects/Automation/auto_deploy.py
@@ -12,39 +12,41 @@ bmp280_ = True
 # Deploy Funktionen BMP180
 def bmp180():
     bmp180_scan = os.system("i2cget -y 1 0x77")
+    print(bmp180_scan)
     if bmp180_scan == 0x00 or 77 or 0:
         # print("bmp180 Container will download", flush=True)
         # os.system("docker pull 326567/bmp180")
-        print("bmp180 Container will deploy", flush=True)
+        print("\nbmp180 Container will deploy", flush=True)
         os.system("docker run --device /dev/i2c-1 326567/bmp180 &")
 
     elif bmp180_scan == "Error: Read failed\n512":
-        print("bmp180 no connection by 512", flush=True)
+        print("\nbmp180 no connection by 512", flush=True)
 
     elif bmp180_scan == "Error: Read failed\n589":
-        print("bmp180 no connection by 589", flush=True)
+        print("\nbmp180 no connection by 589", flush=True)
 
     else:
-        print("bmp180 no connection: unknown message", flush=True)
+        print("\nbmp180 no connection: unknown message", flush=True)
 
 
 # Deploy Funktionen BMP280
 def bmp280():
     bmp280_scan = os.system("i2cget -y 1 0x76")
+    print(bmp280_scan)
     if bmp280_scan == 0x00 or 77 or 0:
         # print("bmp280 Container will download", flush=True)
         # os.system("docker pull 326567/bmp280")
-        print("bmp280 Container will deploy", flush=True)
+        print("\nbmp280 Container will deploy", flush=True)
         os.system("docker run --device /dev/i2c-1 326567/bmp280 &")
 
     elif bmp280_scan == "Error: Read failed\n512":
-        print("bmp180 no connection by 512", flush=True)
+        print("\nbmp180 no connection by 512", flush=True)
 
     elif bmp280_scan == "Error: Read failed\n588":
-        print("bmp180 no connection by 588", flush=True)
+        print("\nbmp180 no connection by 588", flush=True)
 
     else:
-        print("bmp280 no connection: unknown message", flush=True)
+        print("\nbmp280 no connection: unknown message", flush=True)
 
 
 """
