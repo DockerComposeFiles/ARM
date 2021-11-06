3103c71d22aa3d706ec8860f2152a4621ecbb472
Ruben <Rthem@gmx.de>
2021-11-06 20:54:44 +0100

    auto_deploy.py_falsches Deploy Analyse_8

--- projects/Automation/auto_deploy.py
+++ projects/Automation/auto_deploy.py
@@ -8,22 +8,30 @@ import time
 bmp180_ = True
 bmp280_ = True
 
+# Konvertierung des Scan Objektes
+def object_converter(current_object):
+    try:
+        obj = int(current_object)
+    except:
+        obj = 0
+    return obj
 
 # Deploy Funktionen BMP180
 def bmp180():
     bmp180_scan = os.system("i2cget -y 1 0x77")
-    print(bmp180_scan)
-    if bmp180_scan == "0x17" or bmp180_scan == "0" \
-            or bmp180_scan == "0x53" or bmp180_scan == "0x53\n0":
+    bmp180_obj = object_converter(bmp180_scan)
+    print(bmp180_obj)
+    if bmp180_obj == "0x17" or bmp180_obj == "0" \
+            or bmp180_obj == "0x53" or bmp180_obj == "0x53\n0":
         # print("bmp180 Container will download", flush=True)
         # os.system("docker pull 326567/bmp180")
         print("bmp180 Container will deploy\n", flush=True)
         os.system("docker run --device /dev/i2c-1 326567/bmp180 &")
 
-    elif bmp180_scan == "Error: Read failed\n512":
+    elif bmp180_obj == "Error: Read failed\n512":
         print("bmp180 no connection by 512\n", flush=True)
 
-    elif bmp180_scan == "Error: Read failed\n589":
+    elif bmp180_obj == "Error: Read failed\n589":
         print("bmp180 no connection by 589\n", flush=True)
 
     else:
@@ -33,17 +41,18 @@ def bmp180():
 # Deploy Funktionen BMP280
 def bmp280():
     bmp280_scan = os.system("i2cget -y 1 0x76")
-    print(bmp280_scan)
-    if bmp280_scan == "0x17\n0":
+    bmp280_obj = object_converter(bmp280_scan)
+    print(bmp280_obj)
+    if bmp280_obj == "0x17\n0":
         # print("bmp280 Container will download", flush=True)
         # os.system("docker pull 326567/bmp280")
         print("bmp280 Container will deploy\n", flush=True)
         # os.system("docker run --device /dev/i2c-1 326567/bmp280 &")
 
-    elif bmp280_scan == "Error: Read failed":
+    elif bmp280_obj == "Error: Read failed":
         print("bmp180 no connection by 512\n", flush=True)
 
-    elif bmp280_scan == "Error: Read failed\n588":
+    elif bmp280_obj == "Error: Read failed\n588":
         print("bmp180 no connection by 588\n", flush=True)
 
     else:
