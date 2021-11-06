222ba5e052a1b4a469fde1d86382ad1434b33b23
Ruben <Rthem@gmx.de>
2021-11-06 15:48:40 +0100

    auto_deploy.py_8

--- projects/Automation/scan_test.py
+++ projects/Automation/scan_test.py
@@ -1,11 +1,5 @@
 import os
 
-print(os.system("i2cget -V" + "\n"), flush=True)
-
-print(os.system("i2cget -y 1 0x77"), flush=True)
-print(os.system("i2cget -y 1 0x76"), flush=True)
-print(os.system("i2cget -y 1 0x40"), flush=True)
-print("\n", flush=True)
 
 # Konvertierung des Scan Objektes
 def object_converter(current_object):
@@ -15,8 +9,26 @@ def object_converter(current_object):
         obj = 0
     return obj
 
-#Scann des ersten Sensors
+
+# Vergleich
+print(os.system("i2cget -V"), flush=True)
+print("\n", flush=True)
+print(os.system("i2cget -y 1 0x77"), flush=True)
+print(os.system("i2cget -y 1 0x76"), flush=True)
+print(os.system("i2cget -y 1 0x40"), flush=True)
+print("\n", flush=True)
+
+# Scann des 1 Sensors
 obj77 = os.system("i2cget -y 1 0x77")
 res77 = object_converter(obj77)
-
 print(77 + res77, flush=True)
+
+# Scann des 1 Sensors
+obj76 = os.system("i2cget -y 1 0x76")
+res76 = object_converter(obj76)
+print(76 + res76, flush=True)
+
+# Scann des 3 Sensors
+obj40 = os.system("i2cget -y 1 0x40")
+res40 = object_converter(obj40)
+print(40 + res40, flush=True)
