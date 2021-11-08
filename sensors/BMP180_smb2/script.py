e97cf9caee2392b043517297a886d812c03ff477
Ruben <Rthem@gmx.de>
2021-11-08 08:50:16 +0100

    auto_deploy.py_restart-container 4

--- sensors/BMP180_smb2/script.py
+++ sensors/BMP180_smb2/script.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
 # Einbindung der Python Bibliothek
 # Angepasste Form
+import time
+
 import bme280
 
 # Messdaten Holen
 temperatur, druck, x = bme280.readBME280All()
 print("Temperatur : ", temperatur, "C")
 print("Druck: ", druck, "hPa")
+time.sleep(300)
---
+++ sensors/standard/PI-HTU21D/Readme.md
@@ -0,0 +1,3 @@
+# HTU21D-Raspberry
+This container will use Raspberry GPIO
+to connect to HTU21D sensor.
